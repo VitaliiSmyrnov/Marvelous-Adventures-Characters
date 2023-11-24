7411abc62a6c98ee7bbe219464fdd78d87705ae9
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2023-11-24 23:33:32 +0200

    feat: set environments

--- src/services/API.ts
+++ src/services/API.ts
@@ -1,18 +1,17 @@
 import axios from "axios";
 
-const API_KEY = import.meta.env.API_KEY;
 axios.defaults.baseURL = "http://gateway.marvel.com/v1/public";
 axios.defaults.params = {
-  apikey: API_KEY,
-  ts: "",
-  hash: "",
+  apikey: import.meta.env.VITE_API_KEY,
+  ts: import.meta.env.VITE_API_TS,
+  hash: import.meta.env.VITE_API_HASH,
   limit: 8,
 };
 
 const getAllCharacters = async () => {
   const response = await axios.get("/characters");
   console.log("getAllCharacters", response);
-  return response;
+  return response.data.data.results;
 };
 
 export default { getAllCharacters };
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,11 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly API_KEY: string;
+  readonly VITE_API_KEY: string;
+  readonly VITE_API_TS: string;
+  readonly VITE_API_HASH: string;
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
-
-
