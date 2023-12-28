2f06ab9e1aa0809369ad8b43304c4a02ff008ef6
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2023-12-28 13:35:40 +0200

    feat: HomeRandomImage done

---
+++ src/components/Home/HomeRandom/HomeRandomImage/HomeRandomImage.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+import { ICharacters } from "src/modules/IProps";
+
+interface IProps {
+  items: ICharacters[];
+  idx: number;
+}
+
+export const HomeRandomImage: FC<IProps> = ({ items, idx }) => {
+  const { path, extension } = items[idx].thumbnail;
+
+  return (
+    <div className="max-h-[335px] max-w-[335px] rounded md:max-h-[704px] md:max-w-[704px] lg:min-w-[496px] lg:max-w-[496px]">
+      <img
+        src={`${path}.${extension}`}
+        alt="Random character"
+        loading="lazy"
+        className="h-full max-h-full w-full max-w-full rounded object-cover"
+      />
+    </div>
+  );
+};
