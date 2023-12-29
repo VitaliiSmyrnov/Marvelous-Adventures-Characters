c7ae234bce009de6f0e812733ae06992353508c4
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2023-12-29 20:59:28 +0200

    feat: change RandomImage onClick implement

--- src/components/Home/HomeRandom/HomeRandomList/HomeRandomList.tsx
+++ src/components/Home/HomeRandom/HomeRandomList/HomeRandomList.tsx
@@ -4,14 +4,15 @@ import { ICharacters } from "src/modules/IProps";
 
 interface IProps {
   items: ICharacters[];
+  onImageChange: (id: number) => void;
 }
 
-export const HomeRandomList: React.FC<IProps> = ({ items }) => {
+export const HomeRandomList: React.FC<IProps> = ({ items, onImageChange }) => {
   return (
     <ul>
-      {items.map(({ name, description, id }) => {
+      {items.map(({ name, description, id }, idx) => {
         return (
-          <HomeRandomItem key={id} name={name} description={description} />
+          <HomeRandomItem key={id} name={name} description={description} onImageChange={()=> onImageChange(idx)}/>
         );
       })}
     </ul>
